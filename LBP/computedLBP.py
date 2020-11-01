d06b78bd309fd455602f288d5d1442905c9b8b4a
Andrews Sobral <andrewssobral@users.noreply.github.com>
2020-11-01 14:21:28 +0100

    Update computedLBP.py

--- LBP/computedLBP.py
+++ LBP/computedLBP.py
@@ -17,29 +17,17 @@ def compute_LBP(imgnm, lbp_str):
             a = 0.01
             Z = gray_image[ih:ih+neighboor,iw:iw+neighboor] 
             C = Z[1,1]
-            
-            
             Z = Z.astype(np.float32)
             C = C.astype(np.float32)
-        
             s = eval(lbp_str)
-
             lbp = (s >= 0)*1.0
             lbp = lbp.astype(np.uint8)
-            
-        
             img01_vector = lbp.T.flatten()
-        
             img01_vector = np.delete(img01_vector,4)
-        
             ### Convert the binary operated values to a decimal (digit)
             where_lbp_vector = np.where(img01_vector)[0]
-
             num = np.sum(2**where_lbp_vector) if len(where_lbp_vector) >= 1 else 0
             imgLBP[ih+1,iw+1] = num
-## print the LBP values when frequencies are high
-
-            
     return image, gray_image, imgLBP
 
 
